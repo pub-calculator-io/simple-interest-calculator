f842edced068fe786129f7477dee3773243e2c82
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:56:37 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_simple_interest_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Simple Interest Calculator by Calculator.iO";
 
 function display_ci_simple_interest_calculator(){
